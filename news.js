13fa779a9f8bb5beb4e9feef5d16ef9e9d01e04b
MARIOTTO Damien <mariottd@hostname.iut2.upmf-grenoble.fr>
2017-03-17 15:52:38 +0100

    d

--- news.js
+++ news.js
@@ -4,18 +4,24 @@ var recherche_courante_news=[]; // tableau d'objets de type resultats (avec titr
 
 function ajouter_recherche()
 {
-
+	if(recherches.indexOf($("#zone_saisie").val()) == -1){
+		recherches.push($("#zone_saisie").val());
+		$("#recherches-stockees").append
+		("<p class=\"titre-recherche\"><label>"+$("#zone_saisie").val()+"</label><img src=\"croix30.jpg\" class=\"icone-croix\"/> </p>");
+		$("#recherches-stockees:last-child:first-child").attr("onclick","selectionner_recherche(this)");
+		$("#recherches-stockees:last-child:last-child").attr("onclick","supprimer_recherche(this)");
+	}
 }
 
 function supprimer_recherche(e)
-{ 
+{
 
 
 }
 
 
 function selectionner_recherche(e)
-{ 
+{
 
 }
 
@@ -28,37 +34,25 @@ function init()
 
 function recherche_nouvelles()
 {
-	
-	
+
+
 }
 
 
 function maj_resultats(res)
 {
 
-	
+
 }
 
 
 function sauve_news(e)
 {
-	
+
 }
 
 
 function supprime_news(e)
 {
-	
+
 }
-
-
-
-
-
-	
-
-
-
-
-
-
