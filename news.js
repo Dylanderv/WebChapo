26cb663c57f7747c6a588664f13667c187a63296
Unknown <daox008@gmail.com>
2017-03-17 16:42:56 +0100

    qsfsqdf
    qsdf

--- news.js
+++ news.js
@@ -1,3 +1,4 @@
+
 var recherches=[];//tableau contenant des chaines de caracteres correspondant aux recherches stockees
 var recherche_courante;// chaine de caracteres correspondant a la recherche courante
 var recherche_courante_news=[]; // tableau d'objets de type resultats (avec titre, date et url)
@@ -49,6 +50,7 @@ function init(){
 
   }
 }
+
 
 
 function recherche_nouvelles()
