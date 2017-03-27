200ed581da7b05d06fbadee894701ffd8f8ef6a8
DERVAUX Dylan <dervauxd@hostname.iut2.upmf-grenoble.fr>
2017-03-27 14:51:17 +0200

    mdr

--- news.js
+++ news.js
@@ -5,14 +5,17 @@ var recherche_courante_news=[]; // tableau d'objets de type resultats (avec titr
 
 function ajouter_recherche()
 {
-	if(recherches.indexOf($("#zone_saisie").val()) == -1){
-		recherches.push($("#zone_saisie").val());
-		$("#recherches-stockees").append
-		("<p class=\"titre-recherche\"><label>"+$("#zone_saisie").val()+"</label><img src=\"croix30.jpg\" class=\"icone-croix\"/> </p>");
-		$("#recherches-stockees").last().find("label").attr("onclick","selectionner_recherche(this)");
-		$("#recherches-stockees").last().find("img").attr("onclick","supprimer_recherche(this)");
-		var rechercheJSON=JSON.stringify(recherches);
-		$.cookie("recherches",recherches,{ expires : 1000 });
+	var data = $("#zone_saisie").val();
+	if(data != ""){
+		if(recherches.indexOf(data) == -1){
+			recherches.push(data);
+			$("#recherches-stockees").append
+			("<p class=\"titre-recherche\"><label>"+$("#zone_saisie").val()+"</label><img src=\"croix30.jpg\" class=\"icone-croix\"/> </p>");
+			$("#recherches-stockees").last().find("label").attr("onclick","selectionner_recherche(this)");
+			$("#recherches-stockees").last().find("img").attr("onclick","supprimer_recherche(this)");
+			//$.cookie("recherches",recherches,{ expires : 1000 });
+			window.localStorage.setItem("recherches", recherches);
+		}
 	}
 }
 
@@ -24,9 +27,14 @@ function supprimer_recherche(e){
 	pos = recherches.indexOf($(e).html());
 	recherches.splice(pos, 1);
   parent.remove();
-	var rechercheJSON=JSON.stringify(recherches);
-	$.cookie("recherches",recherches,{ expires : 1000 });
-
+	if(recherches.length != 0){
+		var rechercheJSON=JSON.stringify(recherches);
+		//$.cookie("recherches",recherches,{ expires : 1000 });
+		window.localStorage.setItem("recherches", recherches);
+	} else {
+		//$.removeCookie('recherches');
+		window.localStorage.removeItem("recherches");
+	}
 }
 
 
@@ -35,8 +43,10 @@ function selectionner_recherche(e){
  	var zone_recherche = $("#zone_saisie");
  	zone_recherche.val(recherche_courante);
 	$("#resultats").empty();
-	var tab=$.cookie($(e).text());
+	//var tab=$.cookie($(e).text());
+	var tab = window.localStorage.getItem($(e).text());
 	if(tab != undefined){
+		//var resJSON = JSON.parse(tab);
 		var resJSON = JSON.parse(tab);
 		for(var i = 0; i < resJSON.length; i++){
 			$("#resultats").append("<p class=\"titre_result\"><a class=\"titre_news\" href=\""+ resJSON[i].url +"\" target=\"_blank\"> "+resJSON[i].titre +" </a><span class=\"date_news\">"+resJSON[i].date +"</span><span class=\"action_news\" onclick=\"sauve_news(this)\"><img src=\"disk15.jpg\"/></span></p>");
@@ -45,7 +55,8 @@ function selectionner_recherche(e){
 }
 
 function init(){
-  var mdr = $.cookie('recherches');
+  //var mdr = $.cookie('recherches');
+	var mdr = window.localStorage.getItem('recherches');
   if(mdr != undefined){
     recherches = mdr.split(",");
 		for(var i = 0; i < recherches.length; i++){
@@ -54,9 +65,12 @@ function init(){
       $("#recherches-stockees").last().find("label").attr("onclick","selectionner_recherche(this)");
       $("#recherches-stockees").last().find("img").attr("onclick","supprimer_recherche(this)");
 		}
-
   }
-
+	$(document).bind('keypress', function(e) {
+	  if(e.keyCode==13){
+	       $('#nouvelle-recherche').children().last().trigger('click');
+	  }
+  });
 }
 
 
@@ -64,10 +78,12 @@ function init(){
 function recherche_nouvelles()
 {
 	//$("#resultats").empty();
-	$("#wait").css("display","block");
 	var data = $("#zone_saisie").val();
-	recherche_courante = data;
-	$.get("search.php?data="+data,maj_resultats);
+	if(data != ""){
+		$("#wait").css("display","block");
+		recherche_courante = data;
+		$.get("search.php?data="+data,maj_resultats);
+	}
 
 }
 
@@ -78,14 +94,15 @@ function maj_resultats(res)
 	var resJSON = JSON.parse(res);
 	var currRes = $("#resultats").children();
 	$("#resultats").empty();
-	var tab = $.cookie(recherche_courante);
-	console.log(tab);
+	//var tab = $.cookie(recherche_courante);
+	var tab = window.localStorage.getItem(recherche_courante);
 	if(tab != undefined){
+		//var cookieParsed = JSON.parse(tab);
 		var cookieParsed = JSON.parse(tab);
 		for(var j = 0; j < cookieParsed.length; j++){
 			for(var i = 0; i < resJSON.length; i++){
-				if(cookieParsed[j].titre.trim() == decodeEntities(resJSON[i].titre).trim()){
-					$("#resultats").append("<p class=\"titre_result\"><a class=\"titre_news\" href=\""+ cookieParsed[j].url +"\" target=\"_blank\"> "+cookieParsed[j].titre +" </a><span class=\"date_news\">"+cookieParsed[j].date +"</span><span class=\"action_news\" onclick=\"sauve_news(this)\"><img src=\"disk15.jpg\"/></span></p>");
+				if(decodeEntities(cookieParsed[j].titre).trim() == decodeEntities(resJSON[i].titre).trim()){
+					$("#resultats").append("<p class=\"titre_result\"><a class=\"titre_news\" href=\""+ cookieParsed[j].url +"\" target=\"_blank\"> "+cookieParsed[j].titre +" </a><span class=\"date_news\">"+cookieParsed[j].date +"</span><span class=\"action_news\" onclick=\"supprime_news(this)\"><img src=\"disk15.jpg\"/></span></p>");
 					resJSON.splice(i,1);
 				}
 			}
@@ -104,12 +121,14 @@ function sauve_news(e)
 	var date_=$(e).prev().html();
 	var url_=$(e).prev().prev().attr("href");
 	var titre_=$(e).prev().prev().html();
-	var objetAdd = {titre : titre_, date : date_, url: url_};
+	var objetAdd = {titre : titre_.trim(), date : date_, url: url_};
 	if(indexOf(recherche_courante_news, objetAdd) == -1){
 		recherche_courante_news.push(objetAdd);
 	}
 	var rechercheJSON=JSON.stringify(recherche_courante_news);
-	$.cookie(recherche_courante,rechercheJSON,{ expires : 1000 });
+	//$.cookie(recherche_courante,rechercheJSON,{ expires : 1000 });
+	window.localStorage.setItem(recherche_courante, rechercheJSON);
+
 }
 
 
@@ -120,11 +139,13 @@ function supprime_news(e)
 	var date_=$(e).prev().html();
 	var url_=$(e).prev().prev().attr("href");
 	var titre_=$(e).prev().prev().html();
-	var objDel = {titre : titre_, date : date_, url: url_};
+	var objDel = {titre : titre_.trim(), date : date_, url: url_};
 	var indexDel = indexOf(recherche_courante_news, objDel);
 	if(indexDel >= 0){
-		recherche_courante_news.splice(indexDel - 1, 1);
+		recherche_courante_news.splice(indexDel, 1);
 	}
 	var rechercheJSON=JSON.stringify(recherche_courante_news);
-	$.cookie(recherche_courante,rechercheJSON,{ expires : 1000 });
+	//$.cookie(recherche_courante,rechercheJSON,{ expires : 1000 });
+	window.localStorage.setItem(recherche_courante, rechercheJSON);
+
 }
